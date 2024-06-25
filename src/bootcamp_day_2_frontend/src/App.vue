65a45102417441877f24feafc9c92b62d4b4ab98
Rozdymkacz <szymon.dziurowicz@gmail.com>
2024-06-25 17:53:43 +0200

    added rust backend

--- src/bootcamp_day_2_frontend/src/App.vue
+++ src/bootcamp_day_2_frontend/src/App.vue
@@ -7,7 +7,9 @@ async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
   const name = target.querySelector('#name').value;
-  await bootcamp_day_2_backend.greet(name).then((response) => {
+  const last_name = target.querySelector('#last_name').value;
+  const numer = target.querySelector('#number').value;
+  await bootcamp_day_2_backend.greet(name, last_name, Number(numer)).then((response) => {
     greeting.value = response;
   });
 }
@@ -21,6 +23,8 @@ async function handleSubmit(e) {
     <form action="#" @submit="handleSubmit">
       <label for="name">Enter your name: &nbsp;</label>
       <input id="name" alt="Name" type="text" />
+      <input id="last_name" alt="Name" type="text" />
+      <input id="number" alt="Number" type="number" />
       <button type="submit">Click Me!</button>
     </form>
     <section id="greeting">{{ greeting }}</section>
