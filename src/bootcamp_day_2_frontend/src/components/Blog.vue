6aa88ef245e71f4ce8b8e475b701d4f104d55005
Rozdymkacz <szymon.dziurowicz@gmail.com>
2024-06-25 19:40:51 +0200

    added way to list wpisy frontend

---
+++ src/bootcamp_day_2_frontend/src/components/Blog.vue
@@ -0,0 +1,26 @@
+<template>
+      <div>
+            <h1>
+                  <button @click="pobierzWpisy">oddaj wpisy</button>
+                  siema
+                  {{ wpisy }}
+            </h1>
+      </div>
+</template>
+
+<script>
+import { bootcamp_day_2_backend } from 'declarations/bootcamp_day_2_backend/index';
+
+export default {
+      data() {
+            return{
+                 wpisy: [] 
+            }
+      },
+      methods: {
+            async pobierzWpisy(){
+                 this.wpisy = await bootcamp_day_2_backend.oddaj_wpisy()
+            }
+      }
+}
+</script>
